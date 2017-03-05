c8f82ead2e546f6d3aeb54901d00ceeb699e8f32
coutvv <coutvv@gmail.com>
2017-03-05 21:54:58 +0300

    wrong duration for decimalChronoUnits

--- src/main/java/ru/coutvv/detime/temporal/DecimalChronoUnit.java
+++ src/main/java/ru/coutvv/detime/temporal/DecimalChronoUnit.java
@@ -19,48 +19,48 @@ public enum DecimalChronoUnit implements TemporalUnit {
     /**
      * В минуте 100 десятичных секунд
      */
-    MINUTE("minute", Duration.of(100, SECOND)),
+    MINUTE("minute", Duration.ofNanos(100*SECOND.getDuration().toNanos())),
 
     /**
      * В часе 100 десятичных минут
      */
-    HOUR("hour", Duration.of(100, MINUTE)),
+    HOUR("hour", Duration.ofNanos(100 * MINUTE.getDuration().toNanos())),
 
     /**
      * В дне 10 десятичных часов
      */
-    DAY("day", Duration.of(10, HOUR)),
+    DAY("day", Duration.ofNanos(10 * HOUR.getDuration().toNanos())),
 
     /**
      * Декада(она же неделя) состоит из 10 дней
      */
-    DECADE("decade", Duration.of(10, DAY)),
+    DECADE("decade", Duration.ofNanos(10 * DAY.getDuration().toNanos())),
 
     /**
      * Месяц состоит из трёх декад
      */
-    MONTH("names", Duration.of(3, DECADE)),
+    MONTH("names", Duration.ofNanos(3 * DECADE.getDuration().toNanos())),
 
-    /**
+    /** TODO: неправильный дурэйшен
      * Олимпийская неделя обычного года
      * Этот отрезок времени прибавляется к концу года
      */
     OLYMPIC_WEEK("olympicWeek", Duration.of(5, DAY)),
 
-    /**
+    /** TODO: неправильный дурэйшен
      * Олимпийская неделя високосного года
      * Этот отрезок времени прибавляется к концу года
      */
     LEAP_OLYMPIC_WEEK("leapOlympicWeek", Duration.of(6, DAY)),
 
-    /**
+    /** TODO: неправильный дурэйшен
      * Год такой же как и обычный по времени
      * но состоит из 12 десятичных месяцев и
      * олимпийской недели
      */
     YEAR("year", Duration.of(12, MONTH).plus(OLYMPIC_WEEK.duration)),
 
-    /**
+    /** TODO: неправильный дурэйшен
      * Високосный год
      */
     LEAP_YEAR("leapYear", Duration.of(1, YEAR).plus(LEAP_OLYMPIC_WEEK.duration));
